f1b71a5b65a70758d9473b5eedc47b16cdc2c65d
Wojciech Wojtyniak <4743+wojtyniak@users.noreply.github.com>
2025-06-22 18:01:42 -0700

    Basic mcp server

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+from contextlib import asynccontextmanager
+from dataclasses import dataclass
+
+from mcp.server.fastmcp import FastMCP
+
+
+@dataclass
+class AppContext:
+    pass
+
+
+@asynccontextmanager
+async def app_lifespan(app: FastMCP):
+    print("Starting up...")
+    yield
+    print("Shutting down...")
+
+
+mcp = FastMCP("MCP-MCP", lifespan=app_lifespan)
+
+
 def main():
-    print("Hello from mcp-mcp!")
+    try:
+        mcp.run(transport="streamable-http")
+    except KeyboardInterrupt:
+        print("Shutting down...")
+    except Exception as e:
+        print(f"Error: {e}")
+        raise e
 
 
 if __name__ == "__main__":
